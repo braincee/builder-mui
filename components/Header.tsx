285e8dafca0cc59c6ed57fbdba6d47686fdaee05
braincee <kwesi938@gmail.com>
2023-06-28 15:45:05 +0000

    Add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export interface HeaderProps {
+    title: string;
+    subtitle: string;
+  }
+  
+  export function Header(props: HeaderProps) {
+    return (
+      <div className="Header">
+        <header>{props.title}</header>
+        <footer>{props.subtitle}</footer>
+      </div>
+    );
+  }
+  
