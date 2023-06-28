ada6f3ea27fa55024616c411035ddac12a25bd46
braincee <kwesi938@gmail.com>
2023-06-28 15:11:39 +0000

    Add builder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,84 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import '@fontsource/public-sans';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import { builder, Builder, withChildren } from '@builder.io/react';
+import { Header } from '../components/Header';
+import { Button } from '@mui/joy';
+// Initialize builder with your apiKey
+builder.init('29e2b58dd1f648f48966fef7096634b2');
+
+// Register Header component so it's available in the drag-and-drop tool
+Builder.registerComponent(Header, {
+  name: 'Header',
+  inputs: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      type: 'string',
+    },
+  ],
+});
+
+//! Register MUI components so they're available in the drag-and-drop tool
+
+Builder.registerComponent(withChildren(Button), {
+  //! keep the same MuiELEMENT name convention for all MUI components
+  name: 'MuiButton',
+  inputs: [
+    {
+      name: 'variant',
+      type: 'text',
+      enum: ['solid', 'soft', 'outlined', 'plain']
+    },
+    {
+      name: 'size',
+      type: 'text',
+      enum: ['sm', 'md', 'lg']
+    },
+    {
+      name: 'color',
+      type: 'text',
+      enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info']
+    },
+    {
+      name: 'loadingPosition',
+      type: 'text',
+      enum: ['center', 'start', 'end']
+    },
+    {
+      name: 'loadingIndicator',
+      type: 'text'
+    },
+    {
+      name: 'disabled',
+      type: 'boolean'
+    },
+    {
+      name: 'content',
+      type: 'uiBlocks',
+      defaultValue: [],
+    },
+    {
+      name: 'loading',
+      type: 'boolean'
+    },
+    {
+      name: 'aria-label',
+      type: 'string'
+    }
+  ],
+  defaultChildren: [
+    { 
+      '@type': '@builder.io/sdk:Element',
+      component: { name: 'Text', options: { text: 'I am child text block!' } }
+    },
+  ]
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
