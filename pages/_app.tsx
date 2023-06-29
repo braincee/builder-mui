75828ef15f70512edd530941debfd4c3626ae990
Achilles-Dev <solohag92@gmail.com>
2023-06-29 08:11:50 +0000

    Update builder components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '@fontsource/public-sans';
 import type { AppProps } from 'next/app';
 import { builder, Builder, withChildren } from '@builder.io/react';
 import { Header } from '../components/Header';
-import { Button, Checkbox, Input, Radio, Slider, Switch, Textarea, Select, Autocomplete } from '@mui/joy';
+import { Button, Checkbox, Input, Radio, Slider, Switch, Textarea, Select, ButtonGroup } from '@mui/joy';
 
 // Initialize builder with your apiKey
 builder.init('29e2b58dd1f648f48966fef7096634b2');
@@ -347,10 +347,69 @@ Builder.registerComponent(withChildren(Select), {
     },
     
   ],
+  childRequirements: {
+    message: 'You can only put Options',
+    query: {
+      'component.name': { $in: ['Option'] },
+    },
+  },
+
   defaultChildren: [
     {
       '@type': '@builder.io/sdk:Element',
-      component: { name: 'Text', options: { text: 'Default Label' } },
+      component: { name: 'Option', options: { text: 'Option 1' } },
+    },
+  ],
+});
+
+Builder.registerComponent(withChildren(ButtonGroup), {
+  name: 'ButtonGroup',
+  inputs: [
+    {
+      name: 'type',
+      type: 'text'
+    },
+    {
+      name: 'aria-label',
+      type: 'string'
+    },
+    {
+      name: 'variant',
+      type: 'text',
+      enum: ['outlined', 'soft', 'solid', 'plain']
+    },
+    {
+      name: 'indicator',
+      type: 'string'
+    },
+    {
+      name: 'size',
+      type: 'text',
+      enum: ['sm', 'md', 'lg']
+    },
+    {
+      name: 'disabled',
+      type: 'boolean',
+      defaultValue: 'false',
+    },
+    {
+      name: 'spacing',
+      type: 'text',
+    },
+    {
+      name: 'orientation',
+      type: 'text',
+    },
+    {
+      name: 'buttonFlex',
+      type: 'string',
+    },
+    
+  ],
+  defaultChildren: [
+    {
+      '@type': '@builder.io/sdk:Element',
+      component: { name: 'Button', options: { text: 'Button 1', type: 'outlined' } },
     },
   ],
 });
