d75d098b682646b41e8dac423b12e3751bb93139
braincee <kwesi938@gmail.com>
2023-06-29 15:22:24 +0000

    Add feedback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,11 +24,72 @@ Builder.registerComponent(Header, {
   ],
 });
 
-//! Register MUI components so they're available in the drag-and-drop tool
+// future links for child blocks
+// https://www.builder.io/c/docs/custom-components-children
+// https://github.com/BuilderIO/builder/blob/main/examples/next-js-builder-site/src/components/material-table.tsx
+// https://github.com/BuilderIO/builder/blob/main/examples/next-js-builder-site/src/components/material-tabs.tsx
+// https://github.com/BuilderIO/builder/blob/main/examples/next-js-builder-site/src/components/tooltip.tsx
+// https://forum.builder.io/t/custom-builder-react-component-that-needs-redux-data/38/2
+// https://mui.com/material-ui/guides/composition/#wrapping-components
+// https://forum.builder.io/t/cant-use-flex-grid-with-slots/2892/9
+// https://github.com/BuilderIO/builder/blob/dc43381077239c2cdc9c9af8e949796ddf0bf2ac/packages/react/src/blocks/Symbol.tsx#L88-L90
+// https://forum.builder.io/t/creating-a-symbol-with-a-react-component-inside/897/2
+// https://ideas.builder.io/ideas/PROD-I-61
+// Register MUI components so they're available in the drag-and-drop tool
+//need to find the right type for these
+const buildify = (Component: any) => (props: any) => {
+  let customProps = {...props};
+  delete customProps.builderBlock;
+  delete customProps.builderState;
+  delete customProps.attributes;
 
-Builder.registerComponent(withChildren(Button), {
-  //! keep the same MuiELEMENT name convention for all MUI components
-  name: 'MuiButton',
+  const sxStyle = {...props.builderBlock.responsiveStyles?.[props?.builderState?.state?.deviceSize ?? 'large']};
+
+  return (<Component 
+      sx={sxStyle}
+      {...customProps}
+      // https://forum.builder.io/t/custom-component-with-nowrap-true-cause-focus-issue/817/5
+      ref={
+        (el: HTMLElement | null) => {
+          props.attributes &&
+          Object.entries(props.attributes).forEach(([attributeName, attributeValue]) => {
+            if (attributeName.startsWith("builder-")) {
+              el?.setAttribute(attributeName, attributeValue as string); // Here i set the "builder-*" attributes on the element
+            }
+          });
+        }
+      }
+      // {...props.attributes}
+   />)
+  }
+
+//! STEPHEN
+//! Use the buildify function above to wrap each component
+//! This will add the sx prop to each component
+//! Keep the name convention the same, eg MuiButton, MuiCheckbox, etc
+const MuiButton = buildify(Button);
+
+
+Builder.registerComponent(withChildren(MuiButton), {
+  name: 'Button',
+  //! STEPHEN
+  //! Place this on all components, with value = true
+  noWrap: true,
+  //! STEPHEN
+  //! Set this for each component based on your common sense of whether it can have children (e.g. a button can have children, but a switcher can't)
+  canHaveChildren: true, 
+  //! STEPHEN
+  //! https://www.builder.io/c/docs/register-components-options#child-requirements
+  //! Set the childRequirements property to an object with a message property to display a message when a user tries to add a child component that isn't supported.
+  //! For example, a ButtonGroup can only have Button children.
+  childRequirements: {
+    message: 'Only Text components are accepted.',
+    component: 'Text',
+  },
+  //! STEPHEN
+  //! Set this field for components that can only be placed inside a specific parent, not by themselves
+  //! https://www.builder.io/c/docs/register-components-options#requires-parent
+  // requiresParent: {},
   inputs: [
     {
       name: 'variant',
@@ -59,11 +120,6 @@ Builder.registerComponent(withChildren(Button), {
       type: 'boolean'
     },
     {
-      name: 'content',
-      type: 'uiBlocks',
-      defaultValue: [],
-    },
-    {
       name: 'loading',
       type: 'boolean',
     },
@@ -71,6 +127,17 @@ Builder.registerComponent(withChildren(Button), {
       name: 'aria-label',
       type: 'string',
     }
+    //! STEPHEN
+    //! List all the props that have NOT been added to the component
+    //! All props of a components are available in its API page
+    //! Do not list the `sx` prop. It is added automatically by the buildify function above.
+    //! EXAMPLE below
+    // Not added:
+    // action (func)
+    // component (elementType)
+    // endDecorator (node)
+    // slotProps (object)
+    // slots (object)
   ],
   defaultChildren: [
     { 
@@ -258,8 +325,11 @@ Builder.registerComponent(withChildren(Switch), {
       type: 'text',
       enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info']
     },
-
+    //! STEPHEN
+    //! For example, the Switch is not working because `checked` is missing.
   ],
+  //! STEPHEN
+  //! The Switch element should not take any children, it's a Switch.
   defaultChildren: [
     { 
       '@type': '@builder.io/sdk:Element',
@@ -412,3 +482,30 @@ Builder.registerComponent(withChildren(ButtonGroup), {
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+//! TODO
+//! 0. Some props are missing from the current components. I'm not referring to `node` type props, I'm referring to props that should have been present. Like `fullWidth` for Button.
+//! 1. See my comments above (tagged STEPHEN) and apply them to the components you've already built, where applicable
+//! 2. Add IconButton
+//! 3. Add FormControl
+//! 4. Add FormHelperText
+//! 5. Add FormLabel
+//! 6. Add RadioGroup
+//! 7. Add AspectRatio
+//! 8. Add Avatar
+//! 9. Add AvatarGroup
+//! 10. Add SvgIcon
+//! 11. Add Badge
+//! 12. Add Chip
+//! 13. Add ChipDelete
+//! 14. Add Divider
+//! 15. Add Tooltip
+//! 16. Add Typography
+//! 17. Add Alert
+//! 18. Add CircularProgress
+//! 19. Add LinearProgress
+//! 20. Add Sheet
+//! 21. Add Link
+//! 22. Add Grid
+//! 23. Add Stack
+//! DEADLINE: Satuday evening
