4bb9e6ff5bc24c66a4eb925995654a27f569deb9
braincee <kwesi938@gmail.com>
2023-08-30 20:56:16 +0000

    Add Grid, IconButton and LinearProgress

---
+++ components/new/MuiIconButton.tsx
@@ -0,0 +1,63 @@
+import { IconButton } from '@mui/joy'
+import { ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, ...rest } = props
+
+    return <IconButton {...rest}>{children}</IconButton>
+}
+
+const config: ComponentInfo = {
+    name: 'IconButton',
+    noWrap: true,
+    canHaveChildren: true,
+    inputs: [
+        {
+            name: 'color',
+            type: 'text',
+            enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
+        },
+        {
+            name: 'component',
+            type: 'string',
+        },
+        {
+            name: 'disabled',
+            type: 'boolean',
+            defaultValue: false,
+        },
+        {
+            name: 'focusVisibleClassName',
+            type: 'string',
+        },
+        {
+            name: 'size',
+            type: 'text',
+            enum: ['sm', 'md', 'lg'],
+        },
+        {
+            name: 'slotProps',
+            type: 'object',
+            defaultValue: {},
+        },
+        {
+            name: 'variant',
+            type: 'text',
+            enum: ['solid', 'soft', 'outlined', 'plain'],
+        },
+    ],
+
+    defaultChildren: [
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'Icon' },
+        },
+    ],
+}
+
+export default {
+    component,
+    ...config,
+}
