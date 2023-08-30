3657ac41b44f208a331523d980e8b30695648a60
braincee <kwesi938@gmail.com>
2023-08-30 20:28:33 +0000

    Add FormControl, FormHelperText and FormLabel

---
+++ components/new/MuiFormHelperText.tsx
@@ -0,0 +1,38 @@
+import { FormHelperText } from '@mui/joy'
+import { ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, ...rest } = props
+
+    return <FormHelperText {...rest}>{children}</FormHelperText>
+}
+
+const config: ComponentInfo = {
+    name: 'FormHelperText',
+    noWrap: true,
+    canHaveChildren: true,
+	inputs: [
+		{
+		  name: 'component',
+		  type: 'string',
+		},
+		{
+			name: 'slotProps',
+			type: 'object',
+			defaultValue: {},
+		}
+	  ],
+	  defaultChildren: [
+		{
+		  '@type': '@builder.io/sdk:Element',
+		  component: { name: 'Text', options: { text: 'Add text'} },
+		},
+	  ],
+}
+
+export default {
+    component,
+    ...config,
+}
