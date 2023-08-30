dc2a1b1d799194e18594f0a56151fd22435f4a8b
braincee <kwesi938@gmail.com>
2023-08-30 20:15:28 +0000

    Add ChipDelete, CircularProgress and Divider

---
+++ components/new/MuiDivider.tsx
@@ -0,0 +1,53 @@
+import { Divider } from '@mui/joy'
+import { ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, ...rest } = props
+
+    return <Divider {...rest}>{children}</Divider>
+}
+
+const config: ComponentInfo = {
+    name: 'Divider',
+    noWrap: true,
+    canHaveChildren: true,
+    inputs: [
+        {
+            name: 'component',
+            type: 'string',
+        },
+        {
+            name: 'inset',
+            type: 'text',
+            enum: ['none', 'context', 'string'],
+        },
+        {
+            name: 'orientation',
+            type: 'text',
+            enum: ['horizontal', 'vertical'],
+            defaultValue: 'horizontal',
+        },
+        {
+            name: 'slotProps',
+            type: 'object',
+            defaultValue: {},
+        },
+    ],
+    defaultChildren: [
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'Text', options: { text: 'Value' } },
+        },
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'Chip', options: { text: 'Value' } },
+        },
+    ],
+}
+
+export default {
+    component,
+    ...config,
+}
