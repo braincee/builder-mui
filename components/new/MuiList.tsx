2a7e0220294814a344ace811c739f94017717ac7
braincee <kwesi938@gmail.com>
2023-08-31 08:10:28 +0000

    Add List, ListDivider and ListItem

---
+++ components/new/MuiList.tsx
@@ -0,0 +1,63 @@
+import { List } from '@mui/joy'
+import { ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, ...rest } = props
+
+    return <List {...rest}>{children}</List>
+}
+
+const config: ComponentInfo = {
+    name: 'List',
+    noWrap: true,
+    canHaveChildren: true,
+    inputs: [
+        {
+            name: 'color',
+            type: 'text',
+            enum: ['danger', 'info', 'neutral', 'primary', 'success', 'warning'],
+        },
+        {
+            name: 'components',
+            type: 'string',
+        },
+		{
+            name: 'orientation',
+            type: 'text',
+            enum: ['horizontal', 'vertical'],
+            defaultValue: 'horizontal',
+        },
+        {
+            name: 'size',
+            type: 'text',
+            enum: ['sm', 'md', 'lg'],
+        },
+        {
+            name: 'slotProps',
+            type: 'object',
+            defaultValue: {},
+        },
+        {
+            name: 'variant',
+            type: 'text',
+            enum: ['outlined', 'soft', 'solid', 'plain'],
+        },
+		{
+			name: 'wrap',
+			type: 'boolean',
+		},
+    ],
+    defaultChildren: [
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'List' },
+        },
+    ],
+}
+
+export default {
+    component,
+    ...config,
+}
