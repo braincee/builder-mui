cf40f5411e12a9762609e810b6a6ad1c368b501a
braincee <kwesi938@gmail.com>
2023-08-31 09:11:00 +0000

    Add ListSubheader, Menu and MenuButton

---
+++ components/new/MuiMenuButton.tsx
@@ -0,0 +1,124 @@
+import { MenuButton } from '@mui/joy'
+import { Blocks, ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, startDecorator, endDecorator, ...rest } = props
+
+    return (
+        <MenuButton
+            {...rest}
+            startDecorator={<Blocks blocks={startDecorator} />}
+            endDecorator={<Blocks blocks={endDecorator} />}
+        >
+            {children}
+        </MenuButton>
+    )
+}
+
+const config: ComponentInfo = {
+    name: 'MenuButton',
+    noWrap: true,
+    canHaveChildren: true,
+    inputs: [
+        {
+            name: 'color',
+            type: 'text',
+            enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
+        },
+        {
+            name: 'component',
+            type: 'string',
+        },
+        {
+            name: 'disabled',
+            type: 'boolean',
+        },
+        {
+            name: 'endDecorator',
+            type: 'blocks',
+            hideFromUI: true,
+            helperText: 'This is an editable region.',
+            defaultValue: [
+                {
+                    '@type': '@builder.io/sdk:Element',
+                    component: {
+                        name: 'Icon',
+                        options: {
+                            name: 'Edit',
+                        },
+                    },
+                    responsiveStyles: {
+                        large: {
+                            // Styles for the editable section
+                        },
+                    },
+                },
+            ],
+        },
+        {
+            name: 'fullWidth',
+            type: 'boolean',
+			defaultValue: 'false',
+        },
+        {
+            name: 'loading',
+            type: 'boolean',
+			defaultValue: 'false',
+        },
+        {
+            name: 'loadingPosition',
+            type: 'text',
+            enum: ['center', 'start', 'end'],
+        },
+        {
+            name: 'size',
+            type: 'text',
+            enum: ['sm', 'md', 'lg'],
+        },
+		{
+			name: 'slotProps',
+			type: 'object',
+			defaultValue: {},
+		},
+		{
+            name: 'startDecorator',
+            type: 'blocks',
+            hideFromUI: true,
+            helperText: 'This is an editable region.',
+            defaultValue: [
+                {
+                    '@type': '@builder.io/sdk:Element',
+                    component: {
+                        name: 'Icon',
+                        options: {
+                            name: 'Edit',
+                        },
+                    },
+                    responsiveStyles: {
+                        large: {
+                            // Styles for the editable section
+                        },
+                    },
+                },
+            ],
+        },
+        {
+            name: 'variant',
+            type: 'text',
+            enum: ['solid', 'soft', 'outlined', 'plain'],
+        },
+    ],
+    defaultChildren: [
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'MenuButton', options: { text: 'Menu Button' } },
+        },
+    ],
+}
+
+export default {
+    component,
+    ...config,
+}
