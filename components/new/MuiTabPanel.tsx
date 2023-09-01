44048916fec3c3231e6eed67f0ac4fa172d99d77
braincee <kwesi938@gmail.com>
2023-09-01 13:21:11 +0000

    Add Tablist, TabPanel and Tabs

---
+++ components/new/MuiTabPanel.tsx
@@ -0,0 +1,63 @@
+import { TabPanel } from '@mui/joy'
+import { ComponentInfo } from '@builder.io/sdk-react'
+import * as React from 'react'
+import type { JSX } from 'react'
+
+function component(props: any): JSX.Element {
+    const { children, ...rest } = props
+
+    return <TabPanel {...rest}>{children}</TabPanel>
+}
+
+const config: ComponentInfo = {
+    name: 'TabPanel',
+    noWrap: true,
+    canHaveChildren: true,
+    inputs: [
+        {
+            name: 'color',
+            type: 'text',
+            enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
+        },
+        {
+            name: 'component',
+            type: 'string',
+        },
+        {
+            name: 'keepMounted',
+            type: 'boolean',
+            defaultValue: 'false',
+        },
+        {
+            name: 'size',
+            type: 'text',
+            enum: ['sm', 'md', 'lg'],
+        },
+        {
+            name: 'slotProps',
+            type: 'object',
+            defaultValue: {},
+        },
+        {
+            name: 'value',
+            type: 'number',
+            defaultValue: 0,
+        },
+        {
+            name: 'variant',
+            type: 'text',
+            enum: ['solid', 'soft', 'outlined', 'plain'],
+        },
+    ],
+    defaultChildren: [
+        {
+            '@type': '@builder.io/sdk:Element',
+            component: { name: 'TabPanel' },
+        },
+    ],
+}
+
+export default {
+    component,
+    ...config,
+}
