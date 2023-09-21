3794b06985260d80cf48eec2ff0e3c6535c21b5c
Achilles-Dev <solohag92@gmail.com>
2023-09-21 09:30:43 +0000

    Update Select & Option component

--- components/mui/MuiOption.tsx
+++ components/mui/MuiOption.tsx
@@ -3,58 +3,64 @@ import { Option } from '@mui/joy'
 import type { JSX } from 'react'
 
 function component(props: any): JSX.Element {
-    return <Option {...props} />
+  return <Option {...props} />
 }
 
 const config: ComponentInfo = {
-    name: 'Option',
-    canHaveChildren: true,
-    childRequirements: {
-        message: 'You can only put Text',
-        query: {
-            'component.name': { $in: ['Text'] },
-        },
+  name: 'Option',
+  canHaveChildren: true,
+  requiresParent: {
+    message: 'Option must be within a Select',
+    query: {
+      'component.name': { $in: ['Select', 'List'] },
     },
-    inputs: [
-        {
-            name: 'color',
-            type: 'text',
-            enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
-            defaultValue: 'neutral',
-        },
-        {
-            name: 'component',
-            type: 'string',
-        },
-        {
-            name: 'disabled',
-            type: 'boolean',
-        },
-        {
-            name: 'label',
-            type: 'string',
-        },
-        {
-            name: 'slotProps',
-            type: 'object',
-            defaultValue: {},
-        },
-        {
-            name: 'variant',
-            type: 'text',
-            enum: ['solid', 'soft', 'outlined', 'plain'],
-        },
-    ],
-    defaultChildren: [
-        {
-            '@type': '@builder.io/sdk:Element',
-            component: { name: 'Text', options: { text: 'Option 1' } },
-        },
-    ],
-    docsLink: "https://mui.com/joy-ui/api/option/"
+  },
+  childRequirements: {
+    message: 'You can only put Text',
+    query: {
+      'component.name': { $in: ['Text'] },
+    },
+  },
+  inputs: [
+    {
+      name: 'color',
+      type: 'text',
+      enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
+      defaultValue: 'neutral',
+    },
+    {
+      name: 'component',
+      type: 'string',
+    },
+    {
+      name: 'disabled',
+      type: 'boolean',
+    },
+    {
+      name: 'label',
+      type: 'string',
+    },
+    {
+      name: 'slotProps',
+      type: 'object',
+      defaultValue: {},
+    },
+    {
+      name: 'variant',
+      type: 'text',
+      enum: ['solid', 'soft', 'outlined', 'plain'],
+    },
+  ],
+  defaultChildren: [
+    {
+      '@type': '@builder.io/sdk:Element',
+      component: { name: 'Text', options: { text: 'Option 1' } },
+    },
+  ],
+  docsLink: 'https://mui.com/joy-ui/api/option/',
 }
 
 export default {
-    component,
-    ...config,
+  component,
+  ...config,
 }
--- components/mui/MuiSelect.tsx
+++ components/mui/MuiSelect.tsx
@@ -4,212 +4,184 @@ import { Blocks } from '@builder.io/sdk-react'
 import { Select } from '@mui/joy'
 
 function component(props: any): JSX.Element {
-    const { children, startDecorator, endDecorator, ...rest } = props
+  const { children, startDecorator, endDecorator, indicator, ...rest } = props
 
-    return (
-        <Select
-            {...rest}
-            startDecorator={
-                <Blocks
-                    blocks={startDecorator}
-                    parent={props.builderBlock && props.builderBlock.id}
-                    path={'component.options.startDecorator'}
-                />
-            }
-            endDecorator={
-                <Blocks
-                    blocks={endDecorator}
-                    parent={props.builderBlock && props.builderBlock.id}
-                    path={'component.options.endDecorator'}
-                />
-            }
-        >
-            {children}
-        </Select>
-    )
+  return (
+    <Select
+      {...rest}
+      startDecorator={
+        <Blocks
+          blocks={startDecorator}
+          parent={props.builderBlock && props.builderBlock.id}
+          path={'component.options.startDecorator'}
+        />
+      }
+      endDecorator={
+        <Blocks
+          blocks={endDecorator}
+          parent={props.builderBlock && props.builderBlock.id}
+          path={'component.options.endDecorator'}
+        />
+      }
+      indicator={
+        <Blocks
+          blocks={indicator}
+          parent={props.builderBlock && props.builderBlock.id}
+          path={'component.options.indicator'}
+        />
+      }
+    >
+      {children}
+    </Select>
+  )
 }
 
 const config: ComponentInfo = {
-    name: 'Select',
-    noWrap: true,
-    canHaveChildren: true,
-    inputs: [
+  name: 'Select',
+  noWrap: true,
+  canHaveChildren: true,
+  inputs: [
+    {
+      name: 'autoFocus',
+      type: 'boolean',
+    },
+    {
+      name: 'color',
+      type: 'text',
+      enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
+      defaultValue: 'neutral',
+    },
+    {
+      name: 'component',
+      type: 'string',
+    },
+    {
+      name: 'defaultListboxOpen',
+      type: 'boolean',
+    },
+    {
+      name: 'defaultValue',
+      type: 'text',
+    },
+    {
+      name: 'disabled',
+      type: 'boolean',
+    },
+    {
+      name: 'endDecorator',
+      type: 'blocks',
+      hideFromUI: true,
+      helperText: 'This is an editable region.',
+      defaultValue: [
         {
-            name: 'autoFocus',
-            type: 'boolean',
-        },
-        {
-            name: 'color',
-            type: 'text',
-            enum: ['primary', 'neutral', 'danger', 'success', 'warning', 'info'],
-            defaultValue: 'neutral',
-        },
-        {
-            name: 'component',
-            type: 'string',
-        },
-        {
-            name: 'defaultListboxOpen',
-            type: 'boolean',
-        },
-        {
-            name: 'defaultValue',
-            type: 'text',
-        },
-        {
-            name: 'disabled',
-            type: 'boolean',
-        },
-        {
-            name: 'endDecorator',
-            type: 'blocks',
-            hideFromUI: true,
-            helperText: 'This is an editable region.',
-            defaultValue: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: {
-                        name: 'Icon',
-                        options: {
-                            name: 'Edit',
-                        },
-                    },
-                    responsiveStyles: {
-                        large: {
-                            // Styles for the editable section
-                        },
-                    },
-                },
-            ],
-        },
-        {
-            name: 'indicator',
-            type: 'blocks',
-            hideFromUI: true,
-            helperText: 'This is an editable region.',
-            defaultValue: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: {
-                        name: 'Box',
-                        options: {},
-                    },
-                    responsiveStyles: {
-                        large: {
-                            // Styles for the editable section
-                        },
-                    },
-                },
-            ],
-        },
-        {
-            name: 'listboxId',
-            type: 'string',
-        },
-        {
-            name: 'listboxOpen',
-            type: 'bool',
-        },
-        {
-            name: 'name',
-            type: 'string',
-        },
-        {
-            name: 'placeholder',
-            type: 'string',
-        },
-        {
-            name: 'required',
-            type: 'bool',
-        },
-        {
-            name: 'size',
-            type: 'text',
-            enum: ['sm', 'md', 'lg'],
-        },
-        {
-            name: 'startDecorator',
-            type: 'blocks',
-            hideFromUI: true,
-            helperText: 'This is an editable region.',
-            defaultValue: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: {
-                        name: 'Icon',
-                        options: {
-                            name: 'Edit',
-                        },
-                    },
-                    responsiveStyles: {
-                        large: {
-                            // Styles for the editable section
-                        },
-                    },
-                },
-            ],
-        },
-        {
-            name: 'value',
-            type: 'text',
-        },
-        {
-            name: 'variant',
-            type: 'text',
-            enum: ['solid', 'soft', 'outlined', 'plain'],
-        },
-    ],
-    defaultChildren: [
-        {
-            '@type': '@builder.io/sdk:Element',
-            component: {
-                name: 'Option',
-                options: {
-                    value: 'opt-1',
-                },
+          '@type': '@builder.io/sdk:Element',
+          component: {
+            name: 'Icon',
+            options: {
+              name: 'Edit',
             },
-            children: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: { name: 'Text', options: { text: 'Option 1' } },
-                },
-            ],
-        },
-        {
-            '@type': '@builder.io/sdk:Element',
-            component: {
-                name: 'Option',
-                options: {
-                    value: 'opt-2',
-                },
+          },
+          responsiveStyles: {
+            large: {
+              // Styles for the editable section
             },
-            children: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: { name: 'Text', options: { text: 'Option 2' } },
-                },
-            ],
+          },
         },
+      ],
+    },
+    {
+      name: 'indicator',
+      type: 'blocks',
+      hideFromUI: true,
+      helperText: 'This is an editable region.',
+      defaultValue: [
         {
-            '@type': '@builder.io/sdk:Element',
-            component: {
-                name: 'Option',
-                options: {
-                    value: 'opt-3',
-                },
+          '@type': '@builder.io/sdk:Element',
+          component: {
+            name: 'Box',
+            options: {},
+          },
+          responsiveStyles: {
+            large: {
+              // Styles for the editable section
             },
-            children: [
-                {
-                    '@type': '@builder.io/sdk:Element',
-                    component: { name: 'Text', options: { text: 'Option 3' } },
-                },
-            ],
+          },
         },
-    ],
-    docsLink: "https://mui.com/joy-ui/react-select/"
+      ],
+    },
+    {
+      name: 'listboxId',
+      type: 'string',
+    },
+    {
+      name: 'listboxOpen',
+      type: 'bool',
+    },
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'placeholder',
+      type: 'string',
+    },
+    {
+      name: 'required',
+      type: 'bool',
+    },
+    {
+      name: 'size',
+      type: 'text',
+      enum: ['sm', 'md', 'lg'],
+    },
+    {
+      name: 'startDecorator',
+      type: 'blocks',
+      hideFromUI: true,
+      helperText: 'This is an editable region.',
+      defaultValue: [
+        {
+          '@type': '@builder.io/sdk:Element',
+          component: {
+            name: 'Icon',
+            options: {
+              name: 'Edit',
+            },
+          },
+          responsiveStyles: {
+            large: {
+              // Styles for the editable section
+            },
+          },
+        },
+      ],
+    },
+    {
+      name: 'value',
+      type: 'text',
+    },
+    {
+      name: 'variant',
+      type: 'text',
+      enum: ['solid', 'soft', 'outlined', 'plain'],
+    },
+  ],
+  defaultChildren: [
+    {
+      '@type': '@builder.io/sdk:Element',
+      component: { name: 'Option', options: { text: 'Option 1' } },
+    },
+  ],
+  childRequirements: {
+    message: 'You can only put Option in a Select',
+    query: {
+      'component.name': { $in: ['Option'] },
+    },
+  },
+  docsLink: 'https://mui.com/joy-ui/react-select/',
 }
 
 export default {
-    component,
-    ...config,
+  component,
+  ...config,
 }
